06bd380b3f83215534551a63cf458b521b85fedc
Sahil Memon <sahil.infosphere@gmail.com>
2024-05-20 17:19:30 +0530

    Implemented onClick listeners on CardViews to open URLs in a browser and within the app

--- app/src/main/java/com/example/amrozia/AboutUsActivity.java
+++ app/src/main/java/com/example/amrozia/AboutUsActivity.java
@@ -1,16 +1,19 @@
 package com.example.amrozia;
 
-import static androidx.core.view.GravityCompat.*;
+import static androidx.core.view.GravityCompat.START;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.cardview.widget.CardView;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -24,6 +27,9 @@ public class AboutUsActivity extends AppCompatActivity implements NavigationView
 
     // Declare NavigationView
     NavigationView navigationView;
+
+    // Declare CardViews
+    CardView facebookCard, twitterCard, instagramCard, whatsappCard;
 
     // Override the onCreate method which is called when the activity is first created
     @Override
@@ -44,12 +50,58 @@ public class AboutUsActivity extends AppCompatActivity implements NavigationView
         // Initialize the DrawerLayout
         drawerLayout = findViewById(R.id.drawer_layout);
 
+        // Initialize CardViews
+        facebookCard = findViewById(R.id.card_facebook);
+        twitterCard = findViewById(R.id.card_twitter);
+        instagramCard = findViewById(R.id.card_instagram);
+        whatsappCard = findViewById(R.id.card_whatsapp);
+
+        // Set click listeners for the CardViews
+        // Open UrLs in the app installed, otherwise in the default browser
+        facebookCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUrlInApp("https://facebook.com/amrozia.in");
+            }
+        });
+
+        twitterCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUrlInApp("https://twitter.com/globalfashion85");
+            }
+        });
+
+        instagramCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUrlInApp("https://instagram.com/global_fashion_amrozia");
+            }
+        });
+
+        whatsappCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUrlInApp("https://wa.me/+917219777055");
+            }
+        });
+
         // Set an OnClickListener on the grid icon
         // When the grid icon is clicked, the navigation drawer will open
         gridIcon.setOnClickListener(v -> {
             drawerLayout.openDrawer(START);
         });
     }
+
+    // The openUrlInApp method will construct a URL and start an Intent to view the URL.
+    // If the corresponding app (Facebook, Twitter, Instagram, WhatsApp) is installed on the device, the URL will open in the app.
+    // Otherwise, the URL will open in the default web browser
+    private void openUrlInApp(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         NavigationUtils.navigateTo(item, this);
