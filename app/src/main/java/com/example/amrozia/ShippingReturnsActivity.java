6620482212a7d0f9d4fd422a8396c018eaed73ef
Sahil Memon <sahil.infosphere@gmail.com>
2024-05-20 13:32:20 +0530

    Added email intent for contact_support_email

--- app/src/main/java/com/example/amrozia/ShippingReturnsActivity.java
+++ app/src/main/java/com/example/amrozia/ShippingReturnsActivity.java
@@ -3,9 +3,12 @@ package com.example.amrozia;
 import static androidx.core.view.GravityCompat.*;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -48,6 +51,21 @@ public class ShippingReturnsActivity extends AppCompatActivity implements Naviga
         // When the grid icon is clicked, the navigation drawer will open
         gridIcon.setOnClickListener(v -> {
             drawerLayout.openDrawer(START);
+        });
+
+        // Find the contact_support_email button and set OnClickListener
+        findViewById(R.id.contact_support_email).setOnClickListener(v -> {
+            // Create an intent to send an email
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"support@amrozia.in"}); // Email address
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Shipping & Returns Support"); // Subject of the email
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }else {
+                // No email app found, show a toast or handle the situation accordingly
+                Toast.makeText(this, "No email app found", Toast.LENGTH_SHORT).show();
+            }
         });
     }
     @Override
