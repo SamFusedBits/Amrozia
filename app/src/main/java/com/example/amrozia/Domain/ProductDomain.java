6b70df33d195af17c7872004a1cade22f9e26029
Sahil Memon <sahil.infosphere@gmail.com>
2024-06-04 22:45:22 +0530

    Implemented ProductDomain.java for handling product-related data

---
+++ app/src/main/java/com/example/amrozia/Domain/ProductDomain.java
@@ -0,0 +1,82 @@
+package com.example.amrozia.Domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ProductDomain implements Serializable {
+
+    private String id;
+    private String title;
+    private String description;
+    private double price;
+    private ArrayList<String> picUrl;
+    private String category;
+    private ArrayList<String> size;
+
+    // Default constructor is needed for Firebase
+    public ProductDomain() {}
+
+    public ProductDomain(String id, String title, String description, double price, ArrayList<String> size, ArrayList<String> picUrl, String category) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.size = size;
+        this.picUrl = picUrl;
+        this.category = category;
+    }
+
+    // Getters and setters for all fields
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(ArrayList<String> picUrl) {
+        this.picUrl = picUrl;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public ArrayList<String> getSize() {
+        return size;
+    }
+
+    public void setSize(ArrayList<String> size) {
+        this.size = size;
+    }
+
+}
