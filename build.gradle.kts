9b4c820f6fa28616afd8dd1376be0a5838240eb0
Sahil Memon <sahil.infosphere@gmail.com>
2024-08-10 06:49:57 +0530

    Add analytics dependencies in build.gradle.kts(Project)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.2")
+        classpath("com.google.firebase:perf-plugin:1.4.2")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
@@ -9,4 +12,7 @@ plugins {
 
     // Add the dependency for the Google services Gradle plugin
     id("com.google.gms.google-services") version "4.4.2" apply false
+
+    // Add the dependency for the Crashlytics Gradle plugin
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
