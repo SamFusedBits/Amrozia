beceb03d51c72cb1b0c288f64ef30ca186400b66
Sahil Memon <sahil.infosphere@gmail.com>
2024-09-14 10:08:41 +0530

    Add ChangeNumberItemsListener interface to handle cart item count updates

---
+++ app/src/main/java/com/globalfashion/amrozia/Helper/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package com.globalfashion.amrozia.Helper;
+// This interface is used to notify the adapter when the number of items in the cart changes
+public interface ChangeNumberItemsListener {
+    void changed();
+}
